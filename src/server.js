43b99f9da4cd82a4713e16e47cab4fc2a9daa3af
Denis Zaynagabdinov <zaiden.ranepa@gmail.com>
2023-08-11 14:13:14 +0300

    pre-deploy changes

--- src/server.js
+++ src/server.js
@@ -1,6 +1,5 @@
 import path from 'path';
 import express from 'express';
-import morgan from 'morgan';
 import session from 'express-session';
 import store from 'session-file-store';
 import jsxRender from './utils/jsxRender';
@@ -25,12 +24,11 @@ const sessionConfig = {
   },
 };
 
-app.engine('jsx', jsxRender);
-app.set('view engine', 'jsx');
+app.engine('js', jsxRender);
+app.set('view engine', 'js');
 app.set('views', path.join(__dirname, 'components'));
 
 app.use(express.static('public'));
-app.use(morgan('dev'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(session(sessionConfig));
